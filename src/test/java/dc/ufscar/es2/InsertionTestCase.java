4c9545beaa8b91acdbc4fc2d50f7f4f43b1c6923
wpstudart <wpstudart@yahoo.com.br>
2017-07-11 11:44:19 -0300

    Dois testes funcionais

---
+++ src/test/java/dc/ufscar/es2/InsertionTestCase.java
@@ -0,0 +1,50 @@
+package dc.ufscar.es2;
+
+import net.sf.jabref.model.entry.BibEntry;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
+public class InsertionTestCase {
+
+    private BibEntry entry;
+    private BibEntry emptyEntry;
+
+    @Before
+    public void setUp(){
+        entry = new BibEntry();
+
+        // Empty entry for some tests
+        emptyEntry = new BibEntry();
+        emptyEntry.setType("article");
+        emptyEntry.setChanged(false);
+    }
+
+    @After
+    public void tearDown(){
+        entry = null;
+    }
+
+    @Test
+    public void nullBibEntry(){
+        entry.setField("","");
+        Assert.assertTrue(entry.getKeywords(',').isEmpty());
+    }
+
+    @Test
+    public void invalidIdBibEntry(){
+        String id = "99999999999999999999999999999999999999";
+        String expected = "99999999999999999999999999999999999999";
+        entry.setId(id);
+        id = entry.getId();
+        Assert.assertEquals(expected, id);
+    }
+
+    @Test
+    public void testeBibEntry3(){
+
+    }
+
+}
